efb2ed3de6a19643d768247b768b90ca50fbf944
Arya <63557848+Ary4-alt@users.noreply.github.com>
2021-07-26 14:45:07 -0400

    Update Emojis.py

--- Emojis.py
+++ Emojis.py
@@ -1,5 +1,7 @@
+# Defining a function for the conversion of emojis
 def emoji_converter(message):
     words = message.split(" ")
+    # Dictionary that contains the Emojis
     emojis = {
         ":)": "😀",
         ":(": "😥",
@@ -10,11 +12,15 @@ def emoji_converter(message):
         ":o": "😮",
         ":D": "😁"
     }
+    # Leaving a blank space
     output = ""
+    # The process of calling the function and returning a value
     for word in words:
         output += emojis.get(word, word) + " "
     return output
 
-
+# Prompting the user to enter a text
 message = input(">")
+
+#Priniting the result
 print(emoji_converter(message))
